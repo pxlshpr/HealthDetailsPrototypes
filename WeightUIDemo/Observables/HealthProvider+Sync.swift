4f2f39138e0d146c08928216aa2ad9056b334114
Ahmed Khalaf <pxlshpr@gmail.com>
2024-01-14 04:17:51 +0500

    bump

--- WeightUIDemo/Observables/HealthProvider+Sync.swift
+++ WeightUIDemo/Observables/HealthProvider+Sync.swift
@@ -46,65 +46,38 @@ extension HealthProvider {
         for i in days.indices {
 
             let day = days[i]
-            print("Doing date: \(day.date.shortDateString)")
+            print("Syncing HealthKit values for: \(day.date.shortDateString)")
 
             if let weightSamples {
-                var weight: HealthDetails.Weight {
-                    get { days[i].healthDetails.weight }
-                    set { days[i].healthDetails.weight = newValue }
-                }
-                
-                let samples = weightSamples.filter { $0.date.startOfDay == day.date.startOfDay }
-                weight.removeHealthKitQuantitySamples(notPresentIn: samples.notOurs)
-                weight.addNewHealthKitQuantitySamples(from: samples.notOurs)
-                toDelete.append(contentsOf: samples.ours.notPresent(in: weight.measurements))
-                toExport.append(contentsOf: weight.measurements.nonHealthKitMeasurements.notPresent(in: samples.ours))
+                days[i].healthDetails.weight.processHealthKitSamples(weightSamples, for: day.date, toDelete: &toDelete, toExport: &toExport)
             }
 
             if let heightSamples {
-                var height: HealthDetails.Height {
-                    get { days[i].healthDetails.height }
-                    set { days[i].healthDetails.height = newValue }
-                }
-                
-                let samples = heightSamples.filter { $0.date.startOfDay == day.date.startOfDay }
-                height.removeHealthKitQuantitySamples(notPresentIn: samples.notOurs)
-                height.addNewHealthKitQuantitySamples(from: samples.notOurs)
-                toDelete.append(contentsOf: samples.ours.notPresent(in: height.measurements))
-                toExport.append(contentsOf: height.measurements.nonHealthKitMeasurements.notPresent(in: samples.ours))
+                days[i].healthDetails.height.processHealthKitSamples(heightSamples, for: day.date, toDelete: &toDelete, toExport: &toExport)
             }
-            
+
             if let leanBodyMassSamples {
-                var leanBodyMass: HealthDetails.LeanBodyMass {
-                    get { days[i].healthDetails.leanBodyMass }
-                    set { days[i].healthDetails.leanBodyMass = newValue }
-                }
-                
-                let samples = leanBodyMassSamples.filter { $0.date.startOfDay == day.date.startOfDay }
-                leanBodyMass.removeHealthKitQuantitySamples(notPresentIn: samples.notOurs)
-                leanBodyMass.addNewHealthKitQuantitySamples(from: samples.notOurs)
-                toDelete.append(contentsOf: samples.ours.notPresent(in: leanBodyMass.measurements))
-                toExport.append(contentsOf: leanBodyMass.measurements.nonHealthKitMeasurements.notPresent(in: samples.ours))
+                days[i].healthDetails.leanBodyMass.processHealthKitSamples(leanBodyMassSamples, for: day.date, toDelete: &toDelete, toExport: &toExport)
             }
+
+//            if let fatPercentageSamples {
+//                days[i].healthDetails.fatPercentage.processHealthKitSamples(fatPercentageSamples, for: day.date, toDelete: &toDelete, toExport: &toExport)
+//            }
 
             /// [ ] If the day has DietaryEnergyPointSource, RestingEnergySource or ActivieEnergySource as .healthKit, fetch them and set them
-
-            /// [ ] Continue this for all Days
-            /// [ ] Once we're doing, go ahead and delete all the measurements we put aside to be deleted
-            /// [ ] Also export all the measurements we put aside
-
-            saveDayInDocuments(days[i])
         }
         
         print("We have: \(toDelete.count) to delete")
         print("We have: \(toExport.count) to export")
         
+        /// [x] Once we're done, go ahead and delete all the measurements we put aside to be deleted
         if !toDelete.isEmpty {
             let start = CFAbsoluteTimeGetCurrent()
             await HealthStore.deleteMeasurements(toDelete)
             print("Delete took: \(CFAbsoluteTimeGetCurrent()-start)s")
         }
 
+        /// [x] Also export all the measurements we put aside
         if !toExport.isEmpty {
             let start = CFAbsoluteTimeGetCurrent()
             await HealthStore.saveMeasurements(toExport)
@@ -112,12 +85,19 @@ extension HealthProvider {
         }
 
         print("syncWithHealthKitAndRecalculateAllDays took: \(CFAbsoluteTimeGetCurrent()-start)s")
-//        await recalculate(days)
+        await recalculate(days)
     }
 }
 
 extension HealthProvider {
-    func recalculate(_ days: [Day]) async {
+    static func recalculate(_ days: [Day]) async {
+        for i in days.indices {
+            
+            let day = days[i]
+            print("Recalculating day: \(day.date.shortDateString)")
+
+            saveDayInDocuments(days[i])
+        }
         //TODO: We need to:
         /// [ ] Go through each Day in order
         /// [ ] Get the HealthDetails
@@ -141,7 +121,7 @@ extension HealthProvider {
     func setDailyValueType(for healthDetail: HealthDetail, to type: DailyValueType) {
         settingsProvider.settings.setDailyValueType(type, for: healthDetail)
         settingsProvider.save()
-        recalculateAllDays()
+        Self.recalculateAllDays()
     }
     
     func setHealthKitSyncing(for healthDetail: HealthDetail, to isOn: Bool) {
@@ -152,10 +132,25 @@ extension HealthProvider {
         }
     }
     
-    func recalculateAllDays() {
+    static func recalculateAllDays() {
         Task {
             let days = await fetchAllDaysFromDocuments()
             await recalculate(days)
         }
+    }
+}
+
+extension HealthKitSyncable {
+    mutating func processHealthKitSamples(
+        _ samples: [HKQuantitySample],
+        for date: Date,
+        toDelete: inout [HKQuantitySample],
+        toExport: inout [any Measurable]
+    ) {
+        let samples = samples.filter { $0.date.startOfDay == date.startOfDay }
+        removeHealthKitQuantitySamples(notPresentIn: samples.notOurs)
+        addNewHealthKitQuantitySamples(from: samples.notOurs)
+        toDelete.append(contentsOf: samples.ours.notPresent(in: measurements))
+        toExport.append(contentsOf: measurements.nonHealthKitMeasurements.notPresent(in: samples.ours))
     }
 }
